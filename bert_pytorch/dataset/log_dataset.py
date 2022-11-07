822e584866d3a803002e65ad2bff9ed65c95956e
peterbaile <peterchenwflms@gmail.com>
2022-11-06 19:34:35 -0500

    test

--- bert_pytorch/dataset/log_dataset.py
+++ bert_pytorch/dataset/log_dataset.py
@@ -31,7 +31,7 @@ class LogDataset(Dataset):
         self.corpus_lines = len(log_corpus)
 
         self.mask_ratio = mask_ratio
-        self.add_special_tokens = True
+        self.add_special_tokens = False
 
         # self.scalar_i = set()
 
